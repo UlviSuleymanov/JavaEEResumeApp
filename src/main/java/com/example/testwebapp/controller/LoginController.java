e10162cb0453c4996c3d4eb7a196b37f5260775a
Ülvi Süleymanov <ulvi.suleymanov.yusif@gmail.com>
2023-02-16 17:20:10 +0400

    implemented login and logout feature

--- src/main/java/com/example/testwebapp/controller/LoginController.java
+++ src/main/java/com/example/testwebapp/controller/LoginController.java
@@ -1,5 +1,6 @@
 package com.example.testwebapp.controller;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import com.example.testwebapp.util.ControllerUtil;
 import dao.inter.UserDaoInter;
 import entity.User;
@@ -13,27 +14,33 @@ import java.io.IOException;
 @WebServlet(name = "LoginController", value = "/login")
 public class LoginController extends HttpServlet {
     private UserDaoInter userDaoInter = Context.instanceUserDao();
+    private static BCrypt.Verifyer verifyer = BCrypt.verifyer();
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-            request.getRequestDispatcher("login.jsp").forward(request,response);
+        request.getRequestDispatcher("login.jsp").forward(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         try {
-        String email = request.getParameter("email");
-            System.out.println(email);
-        String password = request.getParameter("password");
-            System.out.println(password);
-        User user = userDaoInter.getUserWithEmailAndPassword(email,password);
+            String email = request.getParameter("email");
+            String password = request.getParameter("password");
 
-        if(user==null){
-            throw new IllegalArgumentException("Email or password is incorrect.");
-        }
-        request.getSession().setAttribute("loggedInUser",user);
-        response.sendRedirect("users");
-        }catch (Exception exception){
-            ControllerUtil.erorPage(response,exception);
+            System.out.println("password=" + password);
+            User user = userDaoInter.getUserWithEmail(email);
+            if (user == null) {
+                throw new IllegalArgumentException("Such User Does not exist");
+            }
+            System.out.println("hash=" + user.getPassword());
+            BCrypt.Result result = verifyer.verify(password.toCharArray(), user.getPassword().toCharArray());
+            if (!result.verified) {
+                throw new IllegalArgumentException("Password is incorrect");
+            }
+            request.getSession().setAttribute("loggedInUser", user);
+            response.sendRedirect("users");
+        } catch (Exception exception) {
+            ControllerUtil.erorPage(response, exception);
         }
 
     }
